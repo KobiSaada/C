84715fabe2690d3efca42c012ed74a2470e70e16
KobiSaada <73976733+KobiSaada@users.noreply.github.com>
2020-11-10 16:28:23 +0200

    Update main.c

--- myMath/main.c
+++ myMath/main.c
@@ -11,7 +11,7 @@ int main(){
     float func =0 ,func1 =0 , func2=0;
     
     printf("please enter a real num");
-    scanf("%lf,&in");
+    scanf("%lf",&in);
     func=sub((float)add(Exp(int)in),(float)pow(in,3)),(float)2);
     func1=add((float)mul((float)3,(float)in),(float)mul((float)2,(float)pow(in,2)));
     func2=sub((float)(div((float)mul((float)4,(float)pow(in,3)),(float)5)),(float)(mul((float)2,(float)in)));
