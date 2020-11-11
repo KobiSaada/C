548419e8be8f6103d9e0bcb3c00b9c73f80ccdce
KobiSaada <73976733+KobiSaada@users.noreply.github.com>
2020-11-11 23:28:53 +0200

    Update myMath.h

--- myMath/myMath.h
+++ myMath/myMath.h
@@ -4,13 +4,13 @@
 //
 //  Created by user on 09/11/2020.
 //
+#include<stdio.h>
 
-
-float add(float x , float y);
-float sub(float x , float y);
-double mul(double x , int y);
-double div(double x, int y);
-double Exp(int x);
-double Pow(double x , int y);
+  float add(float x , float y);
+  float sub(float x , float y);
+  double mul(double x , int y);
+  double div(double x, int y);
+  double Exp(int x);
+  double Pow(double x , int y);
 
 
